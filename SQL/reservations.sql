15b88e2c10bae5fe7104fcc13a68952af48a082d
lujo1700 <45765386+lujo1700@users.noreply.github.com>
2020-02-24 09:16:18 +0100

    Removed foreign key attribute

--- SQL/reservations.sql
+++ SQL/reservations.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS reservations(
         reservation_name VARCHAR(255),
         reservation_date DATETIME NOT NULL,
         customer_phone VARCHAR(15),
-        table_nr INTEGER NOT NULL,
         PRIMARY KEY (reservation_id)
 );
 
