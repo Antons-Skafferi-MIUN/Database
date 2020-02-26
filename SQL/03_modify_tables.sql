e4dd3b356e38ee7a72068b013980754ea28119ce
lujo1700 <45765386+lujo1700@users.noreply.github.com>
2020-02-26 10:04:15 +0100

    Added foreign key constraints for order_rows and reservations

--- SQL/03_modify_tables.sql
+++ SQL/03_modify_tables.sql
@@ -77,3 +77,13 @@ ALTER TABLE shifts
 ADD
   CONSTRAINT shift_personnel_FK FOREIGN KEY(personnel_id) REFERENCES personnel(personnel_id);
 --------------------------------------------------------
+ALTER TABLE reservations
+ADD
+  CONSTRAINT reservation_restaurant_table_FK FOREIGN KEY (table_id) REFERENCES restaurant_tables(table_id);
+--------------------------------------------------------------------
+ALTER TABLE order_rows
+ADD
+  CONSTRAINT order_row_order_FK FOREIGN KEY(order_id) REFERENCES orders(order_id),
+  CONSTRAINT order_row_food_FK FOREIGN KEY(food_id) REFERENCES foods(food_id),
+  CONSTRAINT order_row_drink_FK FOREIGN KEY(drink_id) REFERENCES drinks(drink_id),
+  CONSTRAINT order_row_order_change_FK FOREIGN KEY(order_change_id) REFERENCES order_changes(order_change_id);
