c7cb87a0bbdef9832660e1323c5a30074c2b22f6
rote1400 <55484595+rote1400@users.noreply.github.com>
2020-03-06 09:40:47 +0100

    added foreign keys

--- SQL_javadb/foreign_keys.sql
+++ SQL_javadb/foreign_keys.sql
@@ -6,7 +6,21 @@
 ALTER TABLE orders
 ADD
   CONSTRAINT order_restaurant_table_FK FOREIGN KEY(table_id) REFERENCES restaurant_tables(table_id);
--- --------------------------------------------------------
+/----------------------------------------------------------/
 ALTER TABLE shifts
 ADD
   CONSTRAINT shift_personnel_FK FOREIGN KEY(personnel_id) REFERENCES personnel(personnel_id);
+/--------------------------------------------------------/
+ALTER TABLE reservations
+ADD
+  CONSTRAINT reservation_restaurant_table_FK FOREIGN KEY (table_id) REFERENCES restaurant_tables(table_id);
+/--------------------------------------------------------------------/
+ALTER TABLE order_rows
+ADD
+  CONSTRAINT order_row_order_FK FOREIGN KEY(order_id) REFERENCES orders(order_id);
+ALTER TABLE order_rows
+ADD
+  CONSTRAINT order_row_food_FK FOREIGN KEY(food_id) REFERENCES foods(food_id);
+ALTER TABLE order_rows
+ADD
+  CONSTRAINT order_row_drink_FK FOREIGN KEY(drink_id) REFERENCES drinks(drink_id);
